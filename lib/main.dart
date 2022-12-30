ac71e252efd79172146371bcf5961172d1d0f00b
premgwl <premgupta470@gmail.com>
2022-12-30 22:43:39 +0530

    Updated logic to change numbers randomly

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
     appBar: AppBar(
       title: const Text("Dice Game"),
-      backgroundColor: Colors.orangeAccent,
+      backgroundColor: const Color(0xffE26868),
     ),
     body: DicePage(),
-    backgroundColor: Colors.teal,
+    backgroundColor: const Color(0xffFF8787),
   )));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 3;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -22,14 +33,22 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
-              onPressed: () => {},
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () => {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(5) + 1;
+                })
+              },
             ),
           ),
           Expanded(
             child: TextButton(
-              child: Image.asset('images/dice1.png'),
-              onPressed: () => {},
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () => {
+                setState(() {
+                  rightDiceNumber = Random().nextInt(5) + 1;
+                })
+              },
             ),
           ),
         ],
