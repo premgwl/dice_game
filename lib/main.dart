6182dbeb3927dd472f3b47314b794d8c8c540c1a
premgwl <premgupta470@gmail.com>
2023-01-01 21:55:26 +0530

    Added random logic behind function.

--- lib/main.dart
+++ lib/main.dart
@@ -24,31 +24,48 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 3;
 
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(5) + 1;
+      rightDiceNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () => {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(5) + 1;
-                })
-              },
-            ),
+        children: [
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: TextButton(
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                  onPressed: () => {changeDiceFace()},
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                  onPressed: () => {changeDiceFace()},
+                ),
+              ),
+            ],
           ),
-          Expanded(
-            child: TextButton(
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: () => {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(5) + 1;
-                })
-              },
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: OutlinedButton(
+              onPressed: () => changeDiceFace(),
+              style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll<Color>(Color(0xffE26868)),),
+              child: const Text(
+                'Roll the Dices!',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Colors.white),
+              ),
             ),
           ),
         ],
